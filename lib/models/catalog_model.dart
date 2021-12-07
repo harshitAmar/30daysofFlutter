8d42c9a3657d2efd2c74c0c02c3e57531d15f750
AmarjeetSrivastava <lalaharshitamar@example.com>
2021-12-07 12:53:02 +0530

    day 11completed

---
+++ lib/models/catalog_model.dart
@@ -0,0 +1,46 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String imageUrl;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.imageUrl});
+}
+
+final products = [
+  Item(
+    id: "001",
+    name: "Oppo A5s",
+    desc: "Best quality product by oppo",
+    price: 15000,
+    color: "#33505a",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
+  ),
+  Item(
+    id: "001",
+    name: "Oppo A5s",
+    desc: "Best quality product by oppo",
+    price: 15000,
+    color: "#33505a",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
+  ),
+  Item(
+    id: "001",
+    name: "Oppo A5s",
+    desc: "Best quality product by oppo",
+    price: 15000,
+    color: "#33505a",
+    imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc",
+  ),
+];
