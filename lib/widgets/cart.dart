4d031e2fdb923ab32079fc0dfa0ec4619e7c9f9a
AmarjeetSrivastava <lalaharshitamar@example.com>
2021-12-14 10:50:29 +0530

    day 23 completed

---
+++ lib/widgets/cart.dart
@@ -0,0 +1,20 @@
+import 'package:firstapp/models/catalog_model.dart';
+
+class CartModel {
+  late CatalogModel _catalog;
+  final List<int> _ItemIds = [];
+  CatalogModel get catalog => _catalog;
+  set catalog(CatalogModel newCatalog) {
+    _catalog = newCatalog;
+  }
+
+  List<Item> get items => _ItemIds.map((id) => _catalog.getById(id)).toList();
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+  void add(Item item) {
+    _ItemIds.add(item.id);
+  }
+  void remove(Item item){
+    _ItemIds.remove(item.id)
+  }
+}
